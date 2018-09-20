bea743cf0cb2f343243c29821dbff7667510572b
Helmut Granda <helmutgranda@Helmuts-MacBook-Pro.local>
2018-09-19 22:14:08 -0500

    Adding a sample by loading a jpg.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import 'bootstrap';
 
 import './scss/index.scss';
 
+import img from '../public/blurred-background.jpg';
+
 $('#alert').click(() => {
   alert('jQuery works!');
 });
+
+$('#blurred_background').attr('src', img);
 
 // Your jQuery code
